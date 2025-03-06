73abf2112a1a24d815fa6a9cb5e1e928a43093b8
devKahn <gaebok0331@gmail.com>
2025-03-06 22:45:42 +0900

    #1 1.1 클래스

---
+++ Practical_CSharp/Practical_CSharp/Part01_WarmUp/Ch01_OOP/Ch01_00_OOP.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_CSharp.Part01_WarmUp.Ch01_OOP
+{
+    public class Ch01_00_OOP
+    {
+        public static void main()
+        {
+
+
+            #region 1.1.2 클래스의 인스턴스를 생성한다
+
+            // new연산사를 사용해 클래스의 인스턴스를 생성
+            // 인스턴스란 컴퓨터의 메모리상에 확보된 클래스의 실체
+            Product yakkwa = new Product(123, "약과", 180);
+
+            #endregion
+
+            #region 1.1.3 객체를 이용한다.
+
+            // 약과 객체의 상품 가격을 꺼내는 코드
+            int price = yakkwa.Price;
+            // 약과 객체의 세금을 포함한 상품 가격을 꺼내는 코드
+            int taxIncluded = yakkwa.GetPriceIncludeingTax();
+
+            #endregion
+
+            #region 1.1.4 인스턴스는 여러 개 만들 수 있다.
+            // 하나의 클래스로부터 여러 개의 인스턴스를 생성할 수 있다.
+            Product honeyYakkwa = new Product(123, "꿀약과", 250);
+            Product chapssal = new Product(235, "찹쌀떡", 160);
+
+            #endregion
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+}
---
+++ Practical_CSharp/Practical_CSharp/Part01_WarmUp/Ch01_OOP/Ch01_01_Class.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_CSharp.Part01_WarmUp.Ch01_OOP
+{
+    // 상품 클래스
+   public class Product
+    {
+        // 상품 코드
+        public int Code { get; private set; }
+        // 상품 이름
+        public string Name { get; private set; }
+        // 상품 가격(세금이 붙지 않은 가격)
+        public int Price { get; private set; }
+
+        /*
+         * 생성자란?  클래스와 이름이 같은 특수한 메서드
+         */
+        public Product(int code, string name, int price)
+        {
+            this.Code = code;
+            this.Name= name;
+            this.Price= price;
+        }
+
+        // 소비세를 구한다(소비세율은 8%)
+        public int GetTax()
+        {
+            return (int)(Price * 0.08);
+        }
+
+        // 세금을 포함한 가격을 구한다.
+        public int GetPriceIncludeingTax()
+        {
+            return Price + GetTax();
+        }
+    }
+}
